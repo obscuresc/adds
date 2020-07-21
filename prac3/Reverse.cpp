6748d6b51608b0ad689620cfc674e2ac805e93ed
obscuresc <arney.jh@gmail.com>
2020-07-21 17:44:43 +0930

    Started prac3 and reverse class

---
+++ prac3/Reverse.cpp
@@ -0,0 +1,16 @@
+/* Reverse class
+
+Provides functions to reverse string and numeric values using recursion
+
+*/
+
+#include "human.h"
+
+
+int Reverse::reverseDigit(int digit) {
+
+	if(digit < 0) return -1;
+
+
+}
+std::string reverseString();
---
+++ prac3/Reverse.h
@@ -0,0 +1,13 @@
+#ifndef REVERSE_H
+#define REVERSE_H
+
+class Reverse{
+
+	public:
+		int reverseDigit(int);
+		std::string reverseString(std::string);
+
+};
+
+
+#endif
