c365ffb2dec11ebba21fcc8741277f976ba0b68e
obscuresc <arney.jh@gmail.com>
2020-07-21 20:40:18 +0930

    Recursive digit implemented but not tested

--- prac3/Reverse.cpp
+++ prac3/Reverse.cpp
@@ -8,9 +8,19 @@ Provides functions to reverse string and numeric values using recursion
 
 
 int Reverse::reverseDigit(int digit) {
-
+	
+	// don't accept negative values	
 	if(digit < 0) return -1;
 
-
+	// get first digit
+	size_t i = 1;
+	for(i; (digit % (10*i)); i++) {}
+	i--;
+	int firstDigit = digit % (10*i);
+	
+	// recursive call 
+	// multiply return by 10 and add first digit
+	return 10*reverseDigit((digit-firstDigit) % 10) + firstDigit;
+	
 }
 std::string reverseString();
