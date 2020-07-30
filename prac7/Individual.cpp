65eb483e5f8ed649b0bce7802bd81cc24d6c8774
obscuresc <arney.jh@gmail.com>
2020-07-30 21:11:06 +0930

    Started prac 7.

---
+++ prac7/Individual.cpp
@@ -0,0 +1,76 @@
+// Individual.cpp
+
+#include "Individual.h"
+
+
+// load binaryString with given string
+Individual::Individual(std::string input) {
+
+	binaryString = input;
+
+}
+
+
+// fill binaryString with character repeated n times
+Individual::Individual(size_t n) {
+
+	const char defaultBinaryFiller = "0";
+	binaryString = std::string(n, defaultBinaryFiller);
+
+}
+
+
+// get value of internal member, binaryString
+std::string Individual::getString() {
+
+	return binaryString;
+
+}
+
+
+// get value of position within binaryString
+int Individual::getBit(int pos) {
+
+	// function guard
+	if(pos > binaryString.size()) return -1;
+
+	return std::stoi(binaryString.substr(pos, singleCharacterString);
+
+}
+
+
+// flip bit at position with binaryString
+void Individual::flipBit(int pos) {
+
+	// function guard
+	if(pos > binaryString.size()) return;
+
+	if(binaryString.at(pos) == "0") {
+
+		binaryString.replace(pos, singleCharacterString, "1");
+	
+	else {
+
+		binaryString.replace(pos, singleCharacterString, "0");
+
+	}
+	
+}
+
+
+// get size of the binaryString
+int Individual::getLength() {
+
+	return (int) binaryString.size();
+
+}
+
+
+// find the lenth of the longest consecutive sequence of 1s
+int Individual::getMaxOnes() {
+
+	// use strtok to segment on basis of 1s
+	// set best guess = 0
+	// if current token longer than best guess, set best guess = size of current token
+
+}
---
+++ prac7/Individual.h
@@ -0,0 +1,27 @@
+// Individual.h
+
+#ifndef INDIVIDUAL_H
+#define INDIVIDUAL_H
+
+#include <string>
+
+#define singleCharacterString 1
+
+
+class Individual {
+
+	private:
+		std::string binaryString();
+
+	public:
+		Individual(size_t);
+		Individual(std::string);
+		std::string getString();
+		int getBit(int);
+		void flipBit(int);
+		int getMaxOnes();
+		int getLength();
+
+};
+
+#endif // Individual.h
