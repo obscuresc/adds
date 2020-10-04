96dea5bae437bc9076393840574ded8255fd1f86
obscuresc <arney.jh@gmail.com>
2020-10-04 20:50:29 +1030

    Prepped for non-recursive quicksort.

---
+++ prac8/QuickSort.cpp
@@ -0,0 +1,17 @@
+// QuickSort.cpp
+#include "QuickSort.h"
+
+std::vector<int> sort(std::vector<int> list) {
+
+	// spec requires pivot be chosen as the second value in the list
+	// so long as long as list has more than two elements
+	// recursion would be easy but try not to
+	// need pivot value, pivot final location, sublish low element location
+	// sublist high element location, 
+	// while not sorted, use high and low element to map out pivot
+	// 'shuffle' sub list, move sublist high and low with respect to
+	// final pivot location. If final pivot location is left of previous pivot
+	// maybe list of previous pivot values/locations
+	return list;	
+
+}
