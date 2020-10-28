0f207e32ef1512363273d7f0a7e2a296cac122a1
Jack Arney <arney.jh@gmail.com>
2020-10-28 21:13:41 +1030

    Tested all maps for basic function. No test done on program guard yet.

--- prac5/main.cpp
+++ prac5/main.cpp
@@ -14,6 +14,17 @@
 // correct number of arguments, 1 for program name, 20 for vector
 #define correctNumArgs 21 
 
+
+void printVector(std::vector<int>* input) {
+
+	for(size_t i = 0; i < input->size(); i++) {
+		std::cout << input->at(i);
+		if(i != input->size() - 1) std::cout << ", ";
+	}
+	
+	std::cout << std::endl;
+}
+
 int main(int argc, char* argv[]) {
 
 	// program guard
@@ -27,16 +38,50 @@ int main(int argc, char* argv[]) {
 	// atoi is not particularly robust but not interested in fixing it
 	// exclude 0th arg as is the program name
 	std::vector<int> L(20);
-	for(size_t i = 1; i < (size_t) argc; i++)
-		L.at(i) = atoi(argv[i]);
+	#ifdef DEBUG
+		std::cout << "Argv vals" << std::endl;
+	#endif // DEBUG
+	for(size_t i = 1; i < (size_t) argc; i++) {
+
+		#ifdef DEBUG
+			std::cout << argv[i] << ", "; 
+		#endif // DEBUG
+		L.at(i - 1) = atoi(argv[i]);
+	}
+
+	#ifdef DEBUG
+		std::cout << "Argument vector" << std::endl;
+		printVector(&L);
+	#endif // DEBUG
 
 	// create map objects
 	MapAbsoluteValue mav;
 	MapSquare ms;
 	MapTriple mt;
+	
+	// run map square as a test
+	#ifdef DEBUG
+		std::cout << "L after map square for test" << std::endl;
+		std::vector<int> mapSquareResult = ms.map(L);
+		printVector(&mapSquareResult);
+	#endif // DEBUG
 
-	std::vector<int> Ldash;
-	std::cout << "Main.cpp" << std::endl;
+	// first map per spec
+	std::vector<int> Ldash = mav.map(L);
+	#ifdef DEBUG
+		std::cout << "L dash after map abs" << std::endl;
+		printVector(&Ldash);
+	#endif // DEBUG
 
+	// second map per spec
+	Ldash = mt.map(Ldash);
+	#ifdef DEBUG
+		std::cout << "L dash after map triple" << std::endl;
+		printVector(&Ldash);
+	#endif // DEBUG
+	
+	// first filter per spec
+
+	//
 	return 0;
 }
