f6a0f0357f9bb9862c4fd7e3a02f35e6d9c8188c
Jack Arney <arney.jh@gmail.com>
2020-11-14 11:15:24 +1030

    Started Referee.cpp

---
+++ prac3/Referee.cpp
@@ -0,0 +1,17 @@
+// Referee.cpp
+#include "Referee.h"
+
+
+Player* Referee::match(Player* player1, Player* player2) {
+
+	int player1winCount = 0;
+	int player2winCount = 0;
+
+	// each match is five rounds
+	const int numberOfRounds = 5;
+	for(size_t rounds = 0; rounds < numberOfRounds; rounds++) {
+
+
+
+	}
+}
